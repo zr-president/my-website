ff05b95ae86ac8b2ebbbffab5b222d5fc41d0a2a
Zhong Rui <zhongrui@example.com>
2026-07-26 22:38:23 +0800

    修复版本号不显示：SITE_VERSION在daily_data.js加载后才设置DOM

--- daily_data.js
+++ daily_data.js
@@ -327,3 +327,6 @@ var DAILY_QUIZ = {
 // Trigger renders (functions defined in index.html)
 if(typeof renderAllPicks === 'function') renderAllPicks();
 if(typeof renderBriefing === 'function') renderBriefing();
+// Set version (defined above, but DOM only ready after index.html)
+var vel = document.getElementById('siteVersion');
+if(vel) vel.textContent = SITE_VERSION;
